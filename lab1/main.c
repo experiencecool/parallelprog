44c4ea1e15ed9b3e343ebc5226deb8c75be0f32a
experiencecool <gleb0002@gmail.com>
2021-04-07 22:40:49 +0300

    init from w10

---
+++ lab1/main.c
@@ -0,0 +1,24 @@
+#define _POSIX_C_SOURCE 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#define A 240
+
+int main(int argc, char* argv[]) {
+    int i, N;
+    struct timeval T1, T2;
+    long delta_ms;
+    N = atoi(argv[1]);
+    gettimeofday(&T1, NULL);
+    for (i=0; i<50; i++) {
+        srand(i);
+    }
+    gettimeofday(&T2, NULL);
+    delta_ms = 1000*(T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec)/1000;
+    printf("\nN=%d. Milliseconds passed: %ld\n", N, delta_ms);
+    return 0;
+}
+
+void extended_random(unsigned int *seedp, int min, int max) {
+//    double r = (rand_r(seedp) % (max + 1 - min)) + min;
+}
