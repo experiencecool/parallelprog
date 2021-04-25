31b61cacc905da77cf2ea1d52007ff7f082f049e
Gleb Korzan <gleb.korzan@gmail.com>
2021-04-25 14:48:40 +0300

    add build folder to script + gitignore

--- lab1/build.sh
+++ lab1/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+rm -rf ./build
+mkdir ./build
 
-gcc -O3 -Wall -Werror -o lab1-par-1  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=1
-gcc -O3 -Wall -Werror -o lab1-par-2  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=2
-gcc -O3 -Wall -Werror -o lab1-par-3  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=3
-gcc -O3 -Wall -Werror -o lab1-par-4  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=4
-gcc -O3 -Wall -Werror -o lab1-seq  main.c -lm
+gcc -O3 -Wall -Werror -o ./build/lab1-par-1  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=1
+gcc -O3 -Wall -Werror -o ./build/lab1-par-2  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=2
+gcc -O3 -Wall -Werror -o ./build/lab1-par-3  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=3
+gcc -O3 -Wall -Werror -o ./build/lab1-par-4  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=4
+gcc -O3 -Wall -Werror -o ./build/lab1-seq  main.c -lm
 
-clang -O3 -Wall -Werror -o lab1-seq-clang  main.c -lm
-clang -O3 -Wall -Werror -o lab1-par-clang  main.c -lm -ftree-slp-vectorize
+clang -O3 -Wall -Werror -o ./build/lab1-seq-clang  main.c -lm
+clang -O3 -Wall -Werror -o ./build/lab1-par-clang  main.c -lm -ftree-slp-vectorize
